17f0d93e6f867327592f0e8b8d9af95e4d4cc4b6
om bhardwaj <ombhardwaj155@gmail.com>
2025-05-06 17:00:48 +0530

    card dynamic

--- todo_app/src/components/header.jsx
+++ todo_app/src/components/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header(props){
     return(
         <div>
-        <h2 className="todo-header"> {props.title}</h2>
+        <h2 className="todo-header">{props.title}</h2>
         </div>
     )
 }
