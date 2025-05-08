cade612632da398e0a19d4f1556fbf2e95a822db
om bhardwaj <ombhardwaj155@gmail.com>
2025-05-08 12:52:16 +0530

    role snippet

---
+++ practice/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Test from "./components/test.jsx";
+import "../src/style.css"
+
+function App(){
+  return(
+    <>
+    <Test/>
+    </>
+  )
+}
+
+
+export default App;
